fb56cf70687e50537d428f8788a2d72e6697a3d1
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-29 14:53:35 +0200

    adding data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 // Debug
 console.log('JS OK!');
 
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) 
+// e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, 
+// visualizzare nome e immagine di ogni contatto
+
 // Config
 const app = new Vue({
     el: '#',
